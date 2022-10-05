6ceebfc88b884d3ef4d2ce08e604762de2c277c5
HiusJames <112969081+HiusJames@users.noreply.github.com>
2022-10-05 10:10:24 -0600

    Updated main Manage with Yang

--- SortingPolygons/src/ca/sait/Manager/SortingPolygons.java
+++ SortingPolygons/src/ca/sait/Manager/SortingPolygons.java
@@ -1,31 +1,121 @@
 package ca.sait.Manager;
 
 import ca.sait.ProblemDomain.*;
+import ca.sait.ProblemDomain.shape.*;
+import ca.sait.ProblemDomain.prism.*;
+
 import java.io.*;
 import java.util.*;
 
 public class SortingPolygons {
-    static ArrayList<Shape> cone = new ArrayList<>();
-    static ArrayList<Shape> cylinder = new ArrayList<>();
-    static ArrayList<Shape> pyramid = new ArrayList<>();
-    static ArrayList<Prism> squares = new ArrayList<>();
-    static ArrayList<Prism> triangle = new ArrayList<>();
-    static ArrayList<Prism> pentagon = new ArrayList<>();
-    static ArrayList<Prism> octagon = new ArrayList<>();
-
-    static ArrayList<Double> height = new ArrayList<>();
-    static ArrayList<Double> base = new ArrayList<>();
-    static ArrayList<Double> volume = new ArrayList<>();
+    String filepath;
+    String type;
+    String sort;
+    static Shape[] shapes;
+    
 
     public void start(String filepath, String type, String sort) throws IOException {
-        AllSorts s = new AllSorts(type);
-        Scanner in = new Scanner(new File(filepath));
+        this.filepath = filepath;
+        this.type = type;
+        this.sort = sort;
+        System.out.println("Application started");
+        long startTime = System.currentTimeMillis();
+        LoadPolygons();
+        SortPolygons();
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
 
+    public void LoadPolygons() throws IOException{
+        Scanner in = new Scanner(new File(filepath));
+        
         String line = in.nextLine();
         String[] fields = line.split(" ");
         int amount = Integer.parseInt(fields[0]);
+        shapes = new Shape[amount];
         for (int i = 1; i < amount; i++) {
+            String name = fields[3*i-2];
+            switch (name){
+                case "Cylinder":
+                    shapes[i-1] = (new Cylinder(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i]))); 
+                break;
+                case "Cone":
+                    shapes[i-1] = (new Cone(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i]))); 
+                break;
+                case "Pyramid":
+                    shapes[i-1] = (new Pyramid(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i])));
+                break;
+                case "SquarePrism":
+                    shapes[i-1] = (new SquarePrism(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i])));
+                break;
+                case "TriangularPrism":
+                    shapes[i-1] = (new TrianglePrism(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i])));
+                break;
+                case "PentagonalPrism":
+                    shapes[i-1] = (new PentagonPrism(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i])));  
+                break;
+                case "OctagonalPrism":
+                    shapes[i-1] = (new OctagonPrism(Integer.parseInt(fields[3*i-1]), Integer.parseInt(fields[3*i])));
+                break;
+            }
             
+        }
+    }
+
+    public void SortPolygons() {
+        AllSorts s = new AllSorts(type);
+        System.out.print("Sort method: ");
+        switch(sort){
+            case "b":
+            System.out.println("Bubble Sort");
+            s.BubbleSort(shapes);
+            break;
+            case "s":
+            System.out.println("Selection Sort");
+            s.SelectionSort(shapes);
+            break;
+            case "i":
+            System.out.println("Insertion Sort");
+            s.InsertionSort(shapes);
+            break;
+            case "m":
+            System.out.println("Merge Sort");
+            s.MergeSort(shapes, 0, 0);
+            break;
+            case "q":
+            System.out.println("Quick Sort");
+            s.QuickSort(shapes);
+            break;
+            case "z":
+            System.out.println("Custom Sort");
+            s.MySort(shapes);
+            break;
+        }
+        System.out.print("Sort by: ");
+        switch(type) {
+            case "h":
+            System.out.println("Height");
+            break;
+            case "a":
+            System.out.println("Base Area");
+            break;
+            case "v":
+            System.out.println("Volume");
+            break;
+        }
+        for(int i = 0; i < shapes.length; i++) {
+            switch(type) {
+                case "h":
+                System.out.println(shapes[i].getHeight() + " " + shapes[i].toString());
+                break;
+                case "a":
+                System.out.println(shapes[i].calcBaseArea() + " " + shapes[i].toString());
+                break;
+                case "v":
+                System.out.println(shapes[i].calcVol() + " " + shapes[i].toString());
+                break;
+
+            }
         }
     }
 }
